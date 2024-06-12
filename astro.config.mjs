4a4856780bdb56f05d740bc456f5a80a072abe7e
HessamCheraghi <hessam.ch.rose@gmail.com>
2024-06-12 19:55:03 +0330

    fix: rename base url to match repo name

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://hessamcheraghi.github.io",
-  base: "resume",
+  base: "resume-old-v2",
   integrations: [tailwind()],
   devToolbar: {
     enabled: false,
